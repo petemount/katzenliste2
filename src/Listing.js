4aec193eb51c3a7a6cff234e0f83a80729cf5538
Github_Name <petemount@t-online.de>
2024-05-24 18:21:56 +0200

    HA240524_Katzenliste mit random Verfügbarkeit

--- src/Listing.js
+++ src/Listing.js
@@ -1,12 +1,15 @@
 import './styles/Listing.css'
+import Timer from './timer';
 
-function CatListing ({ Name, Preis, imageURL }) {
+
+function CatListing ({ Name, Preis, imageURL}) {
         return (   
          <div className="cat-list"> 
              <img src= {imageURL}
              alt="alternativtext" style={{width: '300px', height: 'auto'}}/>
-             <h2>Rasse: {Name} </h2>
-             <h2>Preis: €{Preis}</h2>
+             <p>Rasse: {Name} </p>
+             <p>Preis: €{Preis}</p>
+             < Timer />
              <div style={{border: '7px solid black'}}/>            
          </div>
      )
